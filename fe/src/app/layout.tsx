fcb049f3de8cdcd8419f83583cc247f5b6aa3804
윤진 <151013932+maruru301@users.noreply.github.com>
2025-12-22 23:16:56 +0900

    feat: Navigation을 모바일에서는 sticky, PC에서는 fixed로 반응형 고정 배치 적용 #2

--- fe/src/app/layout.tsx
+++ fe/src/app/layout.tsx
@@ -17,13 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="h-screen flex flex-col">
+      <body className="min-h-screen flex flex-col">
         <Providers>
           <Header/>
           
           <div className="pt-16 flex flex-1 flex-col md:flex-row">
             <Navigation/>
-            <main className="flex-1 flex justify-center items-center">{children}</main>
+            <main className="flex-1 flex justify-center items-center md:pl-28">{children}</main>
           </div>
         </Providers>
       </body>
--- fe/src/components/layout/Navigation.tsx
+++ fe/src/components/layout/Navigation.tsx
@@ -2,7 +2,7 @@ import NavItem from "../common/NavItem"
 
 const Navigation = () => {
   return (
-    <nav className="w-full p-2 md:w-28 md:h-full bg-neutral-100">
+    <nav className="sticky top-16 w-full p-2 md:fixed md:h-full md:w-28 bg-neutral-100">
         <div className="flex flex-wrap text-sm gap-2 justify-center items-center md:flex-col">
             <NavItem label="홈" href="/"/>
             <NavItem label="내역" href=""/>
