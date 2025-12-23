82cc131a4b8b2552965bf8f3b2175accbc8e9370
윤진 <151013932+maruru301@users.noreply.github.com>
2025-12-23 15:40:24 +0900

    design: Footer 수정 #2

--- fe/src/components/layout/Footer.tsx
+++ fe/src/components/layout/Footer.tsx
@@ -1,6 +1,12 @@
 const Footer = () => {
   return (
-    <footer className="w-full h-16 z-10 border-t bg-secondary">Footer</footer>
+    <footer className="w-full h-16 z-10 bg-secondary">
+      <div className="h-full flex justify-center items-center">
+        <p className="text-center text-xs text-muted-foreground">
+            &copy; {new Date().getFullYear()} Spenny. All rights reserved.
+          </p>
+      </div>
+    </footer>
   )
 }
 
