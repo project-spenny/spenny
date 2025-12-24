2b51bc01336e95b0404fcce0e598b6f59faa74f5
윤진 <151013932+maruru301@users.noreply.github.com>
2025-12-24 14:45:40 +0900

    feat: DrawerBottom 내역 리스트에 ScrollArea 적용 #9

--- fe/src/components/DrawerBottom.tsx
+++ fe/src/components/DrawerBottom.tsx
@@ -13,6 +13,7 @@ import {
 } from '@/components/ui/drawer';
 
 import { Button } from '@/components/ui/button';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 export function DrawerBottom() {
   return (
@@ -39,32 +40,34 @@ export function DrawerBottom() {
         </DrawerHeader>
 
         {/* 콘텐츠 영역: 거래 내역 영역 */}
-        <div className="flex flex-col gap-4 p-6">
-          {/* 예시로 3개 */}
-          {[1, 2, 3, 4].map((i) => (
-            <div
-              key={i}
-              className="bg-secondary/80 flex w-full items-center justify-between rounded-lg border px-8 py-6"
-            >
-              {/* 왼쪽 (금액/카테고리) */}
-              <div className="flex flex-col gap-2">
-                <div className="text-xl font-bold">-18,000</div>
-                <DrawerDescription className="text-base font-semibold">
-                  교통비
-                </DrawerDescription>
+        <ScrollArea className="flex-1 overflow-y-auto">
+          <div className="flex flex-col gap-4 p-6">
+            {/* 예시로 3개 */}
+            {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
+              <div
+                key={i}
+                className="bg-secondary/80 flex w-full items-center justify-between rounded-lg border p-6"
+              >
+                {/* 왼쪽 (금액/카테고리) */}
+                <div className="flex flex-col gap-2">
+                  <div className="text-xl font-bold">-18,000</div>
+                  <DrawerDescription className="text-base font-semibold">
+                    교통비
+                  </DrawerDescription>
+                </div>
+                {/* 오른쪽 (상세 내역 보기)) */}
+                <div>
+                  <Button
+                    variant="outline"
+                    className="bg-background/60 h-15 w-15 cursor-pointer rounded-full shadow-md"
+                  >
+                    <ChevronRight />
+                  </Button>
+                </div>
               </div>
-              {/* 오른쪽 (상세 내역 보기)) */}
-              <div>
-                <Button
-                  variant="outline"
-                  className="bg-background/60 h-15 w-15 cursor-pointer rounded-full shadow-md"
-                >
-                  <ChevronRight />
-                </Button>
-              </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        </ScrollArea>
 
         {/* 푸터 영역: 이번 달 거래 내역 버튼 */}
         <DrawerFooter className="shrink-0 border-t p-6">
