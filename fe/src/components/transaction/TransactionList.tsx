7fd7f14987f8ef6abdc303754185fccb2c8b7b10
HyeongjinKim98 <201721157@sangmyung.kr>
2025-12-26 01:57:10 +0900

    feat : 가계부 데이터 리스트 출력

--- fe/src/components/transaction/TransactionList.tsx
+++ fe/src/components/transaction/TransactionList.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
@@ -10,6 +10,23 @@ import {
   ItemContent,
   ItemTitle,
 } from "@/components/ui/item"
+import { toast } from "sonner"
+import { supabase } from "@/utils/supabase/client"
+
+interface Transaction {
+  id: string
+  title: string
+  user_id : string
+  category_id: string
+  type: 'income' | 'expense'
+  date: string
+  amount: number
+  fixed_rule_id : string|null
+  memo : string | null
+  created_at : Date
+  updated_at : Date
+  tags : string[]
+}
 
 const dummyTransactions = [
   { title:'점심 식사', category_id: 'cat-001', type: 'expense', date: '2024-12-23', amount: 15000 },
@@ -23,10 +40,36 @@ const dummyTransactions = [
 
 
 export const TransactionList = () => {
+
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+    useEffect(()=>{
+        const fetchTransactions = async()=>{
+            try{
+                const {data:{user}, error: authError } = await supabase.auth.getUser();
+                if(!user||authError ){
+                    toast.warning('로그인이 필요합니다')
+                    return;
+                }
+                const {data, error} = await supabase.from('transactions')
+                .select('*')
+                .eq('user_id',user.id)
+
+                setTransactions(data || [])
+            }catch(err){
+                toast('데이터를 불러오는 데 실패했습니다')
+            }
+        }
+        fetchTransactions();
+    },[])
+
+
+
+
     return (
         <div className="space-y-6 max-w-md mx-auto p-6">
             <Label className="text-xl">가계부</Label>
-            {dummyTransactions.map((e,index)=>(
+            {transactions.map((e,index)=>(
             <Item variant="outline" key={index}>
                 <ItemContent className="flex flex-row items-center">
                     <div className="flex flex-col gap-1 w-24">
