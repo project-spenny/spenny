69dd974e878a9ba44a9699800ff12e0bd7afc2fa
aroin <bboroin917@gmail.com>
2025-12-29 17:43:54 +0900

    feat: 고정비 관리 페이지 기본 구조 추가 #24

---
+++ fe/src/components/fixed-costs/FixedCostsPage.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+export default function FixedCostsPage() {
+  return (
+    <div className="space-y-4">
+      <header className="flex">
+        <div className="space-y-1">
+          <h1 className="text-xl font-semibold">고정비 관리</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
