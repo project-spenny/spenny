eaade5cb4ce4dcf7e96bb97bee982eafd4786dd5
aroin <bboroin917@gmail.com>
2025-12-30 16:47:24 +0900

    refactor: TypeSelector 타입 개선 #28

--- fe/src/components/transaction/common/TypeSelector.tsx
+++ fe/src/components/transaction/common/TypeSelector.tsx
@@ -1,35 +1,42 @@
-import { Label } from "@/components/ui/label";
-import { cn } from "@/lib/utils";
-export const TypeSelector = ({value, onChange}: {value: string; onChange: (type: string) => void})=>{
-    return(
-        <div className="space-y-2">
-            <Label>거래 유형</Label>
-            <div className="grid grid-cols-2 gap-4">
-                <button
-                    type="button"
-                    onClick={() => onChange('income')}
-                    className={cn(
-                        "px-6 py-3 rounded-lg border-2 transition-all font-medium cursor-pointer",
-                        value === 'income'
-                            ? "border-gray-500"
-                            : "border-gray-300 hover:border-gray-400"
-                    )}
-                >
-                    수입
-                </button>
-                <button
-                    type="button"
-                    onClick={() => onChange('expense')}
-                    className={cn(
-                        "px-6 py-3 rounded-lg border-2 transition-all font-medium cursor-pointer",
-                        value === 'expense'
-                            ? "border-gray-500"
-                            : "border-gray-300 hover:border-gray-400"
-                    )}
-                >
-                    지출
-                </button>
-            </div>
-        </div>
-    )
-}
+import { Label } from '@/components/ui/label';
+import { cn } from '@/lib/utils';
+import type { FixedCostType } from '@/types/fixed-costs';
+
+type Props = {
+  value: FixedCostType;
+  onChange: (type: Exclude<FixedCostType, ''>) => void; // ''은 선택 결과로 나오면 안 됨
+};
+
+export const TypeSelector = ({ value, onChange }: Props) => {
+  return (
+    <div className="space-y-2">
+      <Label>거래 유형</Label>
+      <div className="grid grid-cols-2 gap-4">
+        <button
+          type="button"
+          onClick={() => onChange('income')}
+          className={cn(
+            'cursor-pointer rounded-lg border-2 px-6 py-3 font-medium transition-all',
+            value === 'income'
+              ? 'border-gray-500'
+              : 'border-gray-300 hover:border-gray-400'
+          )}
+        >
+          수입
+        </button>
+        <button
+          type="button"
+          onClick={() => onChange('expense')}
+          className={cn(
+            'cursor-pointer rounded-lg border-2 px-6 py-3 font-medium transition-all',
+            value === 'expense'
+              ? 'border-gray-500'
+              : 'border-gray-300 hover:border-gray-400'
+          )}
+        >
+          지출
+        </button>
+      </div>
+    </div>
+  );
+};
