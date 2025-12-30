dc8928058505d1e782cd21ee743d874fe6ce0a1d
윤진 <151013932+maruru301@users.noreply.github.com>
2025-12-30 10:35:23 +0900

    feat: 전월 지출 데이터 조회 및 총 지출액 비교 로직 구현 #19

--- fe/src/components/analysis/ExpenseAnalysis.tsx
+++ fe/src/components/analysis/ExpenseAnalysis.tsx
@@ -9,7 +9,14 @@ import { toast } from 'sonner';
 
 const ExpenseAnalysis = ({ selectedDate }: { selectedDate: Date }) => {
   const [expenses, setExpenses] = useState<ITransaction[]>([]);
+  const [prevExpenses, setPrevExpenses] = useState<ITransaction[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  const lastMonthDate = new Date(
+    selectedDate.getFullYear(),
+    selectedDate.getMonth() - 1,
+    1
+  );
 
   useEffect(() => {
     const fetchExpenses = async () => {
@@ -17,6 +24,8 @@ const ExpenseAnalysis = ({ selectedDate }: { selectedDate: Date }) => {
         setIsLoading(true);
 
         const { startDate, endDate } = getMonthRange(selectedDate);
+        const { startDate: prevStart, endDate: prevEnd } =
+          getMonthRange(lastMonthDate);
 
         // Supabase에서 현재 로그인한 유저 정보 가져오기
         const {
@@ -31,18 +40,28 @@ const ExpenseAnalysis = ({ selectedDate }: { selectedDate: Date }) => {
         }
 
         // 지출(expense) 타입이고, 해당 기간 내에 작성된 현재 유저의 기록만 조회
-        const { data, error } = await supabase
-          .from('transactions')
-          .select('*')
-          .eq('user_id', user.id)
-          .eq('type', 'expense')
-          .gte('date', startDate)
-          .lte('date', endDate);
+        const [currentMonthRes, lastMonthRes] = await Promise.all([
+          supabase
+            .from('transactions')
+            .select('*')
+            .eq('user_id', user.id)
+            .eq('type', 'expense')
+            .gte('date', startDate)
+            .lte('date', endDate),
+          supabase
+            .from('transactions')
+            .select('*')
+            .eq('user_id', user.id)
+            .eq('type', 'expense')
+            .gte('date', prevStart)
+            .lte('date', prevEnd),
+        ]);
 
-        if (error) throw error;
+        if (currentMonthRes.error) throw currentMonthRes.error;
+        if (lastMonthRes.error) throw lastMonthRes.error;
 
-        console.log(data);
-        setExpenses(data || []);
+        setExpenses(currentMonthRes.data || []);
+        setPrevExpenses(lastMonthRes.data || []);
       } catch (err) {
         console.error('데이터 호출 중 오류 발생', err);
         toast('데이터를 불러오는 데 실패했습니다');
@@ -55,7 +74,12 @@ const ExpenseAnalysis = ({ selectedDate }: { selectedDate: Date }) => {
   }, [selectedDate]);
 
   const totalAmount =
-    expenses?.reduce((sum, item) => sum + (item.amount || 0), 0) || 0;
+    expenses.reduce((sum, item) => sum + (item.amount || 0), 0) || 0;
+  const lastMonthTotal = prevExpenses.reduce(
+    (sum, item) => sum + (item.amount || 0),
+    0
+  );
+  const diff = totalAmount - lastMonthTotal;
 
   return (
     <div className="px-4 py-2">
@@ -64,6 +88,11 @@ const ExpenseAnalysis = ({ selectedDate }: { selectedDate: Date }) => {
         <div>
           <span className="text-red-400">{totalAmount.toLocaleString()}</span>원
         </div>
+      </div>
+
+      <div>
+        지난달보다 <span>{Math.abs(diff).toLocaleString()}</span>원{' '}
+        {diff > 0 ? '더 썼어요' : '아꼈어요'}
       </div>
     </div>
   );
