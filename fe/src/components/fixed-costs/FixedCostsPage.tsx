e29727abdd2a425e489cdf5be54e09ea3ccdef85
aroin <bboroin917@gmail.com>
2025-12-30 10:18:22 +0900

    feat: 고정비 추가 버튼 추가 #24

--- fe/src/components/fixed-costs/FixedCostsPage.tsx
+++ fe/src/components/fixed-costs/FixedCostsPage.tsx
@@ -1,8 +1,11 @@
+import FixedCostsAddButton from './FixedCostsAddButton';
 import FixedCostsList from './FixedCostsList';
 
 export default function FixedCostsPage() {
   return (
-    <div className="mx-auto min-h-screen w-full max-w-lg space-y-6 p-4 md:p-6 lg:p-8">
+    <div className="mx-auto w-full max-w-lg space-y-6 p-4 md:p-6 lg:p-8">
+      <FixedCostsAddButton />
+
       <header>
         <h1 className="text-xl font-semibold">고정비 관리</h1>
       </header>
