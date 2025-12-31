b801b9ffe41da61286bfe13214de89afdde97ffb
HyeongjinKim98 <201721157@sangmyung.kr>
2025-12-31 14:59:11 +0900

    feat : transaction 서버액션 생성

---
+++ fe/src/app/(app)/history/actions.ts
@@ -0,0 +1,57 @@
+'use server';
+
+import { createClient } from '@/utils/supabase/server';
+import { revalidatePath } from 'next/cache';
+
+interface TransactionFilters {
+  type?: 'income' | 'expense';
+  category_id?: string;
+  startDate?: string;
+  endDate?: string;
+  searchQuery?: string;
+}
+
+export const getTransaction = async (filters?: TransactionFilters) => {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) throw new Error('No User');
+
+  let query = supabase
+    .from('transactions')
+    .select('*')
+    .eq('user_id', user.id)
+    .order('date', { ascending: false });
+
+  if (filters?.type) {
+    query = query.eq('type', filters.type);
+  }
+
+  if (filters?.category_id) {
+    query = query.eq('category_id', filters.category_id);
+  }
+
+  if (filters?.startDate) {
+    query = query.eq('startDate', filters.startDate);
+  }
+
+  if (filters?.endDate) {
+    query = query.eq('endDate', filters.endDate);
+  }
+
+  if (filters?.searchQuery) {
+    query = query.ilike('title', `%${filters.searchQuery}%`);
+  }
+
+  const { data, error } = await query;
+  if (error) throw error;
+
+  return data;
+};
+
+export async function revalidateTransactions() {
+  revalidatePath('/history');
+}
