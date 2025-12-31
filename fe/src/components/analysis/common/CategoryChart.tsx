5006a59736f5b0367c791c2bf378df8ffa86d0a1
윤진 <151013932+maruru301@users.noreply.github.com>
2025-12-31 14:58:43 +0900

    feat: Chart.js Colors 플러그인 적용하여 자동 색상 활성화 #31

--- fe/src/components/analysis/common/CategoryChart.tsx
+++ fe/src/components/analysis/common/CategoryChart.tsx
@@ -1,9 +1,15 @@
-import { ArcElement, Chart as ChartJS, Legend, Tooltip } from 'chart.js';
+import {
+  ArcElement,
+  Chart as ChartJS,
+  Colors,
+  Legend,
+  Tooltip,
+} from 'chart.js';
 
 import { Doughnut } from 'react-chartjs-2';
 
 // Chart.js에 필요한 요소들을 등록
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip, Legend, Colors);
 
 type CategoryChartProps = {
   data: {
@@ -20,13 +26,6 @@ const CategoryChart = ({ data }: CategoryChartProps) => {
       {
         label: '금액',
         data: data.map((item) => item.amount),
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.6)',
-          'rgba(153, 102, 255, 0.6)',
-          'rgba(54, 162, 235, 0.6)',
-          'rgba(75, 192, 192, 0.6)',
-          'rgba(255, 206, 86, 0.6)',
-        ],
         borderWidth: 2,
       },
     ],
