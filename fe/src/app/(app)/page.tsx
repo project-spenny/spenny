86ce79fd8c1f62a4387c7d40080cd23f1c7f96a7
HyeongjinKim98 <201721157@sangmyung.kr>
2026-01-02 17:42:03 +0900

    feat : 캘린더 월별 데이터 URL 기반 상태관리

--- fe/src/app/(app)/page.tsx
+++ fe/src/app/(app)/page.tsx
@@ -1,44 +1,54 @@
 import { Calendar } from '@/components/calendar/Calendar';
 import { getTransaction } from './history/actions';
 import { TransactionFilters } from './history/actions';
-import { TransactionList } from '@/components/transaction/TransactionList';
 import { Suspense } from 'react';
+import { formatMonth } from '@/utils/date';
+import { getMonthRange } from '@/utils/date';
 
 interface PageProps {
   searchParams: Promise<{
-    type?: string;
-    category_id?: string;
-    start_date?: string;
-    end_date?: string;
-    queryString?: string;
+    month?: string;
+    selected_date?: string;
   }>;
 }
 
 export default async function Home({ searchParams }: PageProps) {
   const params = await searchParams;
 
+  const currentMonth = params.month || formatMonth(new Date());
+  const monthDate = new Date(`${currentMonth}-01`);
+  const { startDate, endDate } = getMonthRange(monthDate);
+
   const filters: TransactionFilters = {
-    type: params.type as 'income' | 'expense' | undefined,
-    category_id: params.category_id,
-    start_date: params.start_date,
-    end_date: params.end_date,
-    searchQuery: params.queryString,
+    start_date: startDate,
+    end_date: endDate,
   };
+
   return (
     <div>
-      <Suspense fallback={<div>skeleton ui</div>}>
-        <TransactionWrapper filters={filters} />
+      <Suspense fallback={<div>skeleton</div>}>
+        <DataCalendar
+          filters={filters}
+          currentMonth={currentMonth}
+          selectedDate={params.selected_date}
+        />
       </Suspense>
-      <Calendar />
     </div>
   );
 }
 
-async function TransactionWrapper({
+async function DataCalendar({
   filters,
+  currentMonth,
+  selectedDate,
 }: {
   filters: TransactionFilters;
+  currentMonth: string;
+  selectedDate?: string;
 }) {
   const transactions = await getTransaction(filters, true);
-  return <TransactionList transactions={transactions} />;
+  console.log(transactions);
+  console.log(currentMonth);
+  console.log(selectedDate);
+  return <Calendar />;
 }
