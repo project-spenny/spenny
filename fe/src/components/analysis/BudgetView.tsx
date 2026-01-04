7cb8fd499515f63cac58618fbcc8bbab174d9616
윤진 <151013932+maruru301@users.noreply.github.com>
2026-01-04 15:17:45 +0900

    feat: 총 예산 유무에 따른 BudgetView 조건부 렌더링 구현 #46
    - totalBudget이 없을 경우 AnalysisEmpty를 사용하여 설정 유도
    - 예산 데이터가 있을 경우 총 예산 표시

--- fe/src/components/analysis/BudgetView.tsx
+++ fe/src/components/analysis/BudgetView.tsx
@@ -1,9 +1,13 @@
-import AnalysisLoading from './common/AnalysisLoading';
-import AnalysisSection from './common/AnalysisSection';
+import AnalysisEmpty from '@/components/analysis/common/AnalysisEmpty';
+import AnalysisLoading from '@/components/analysis/common/AnalysisLoading';
+import AnalysisSection from '@/components/analysis/common/AnalysisSection';
+import { Button } from '@/components/ui/button';
+import { Calculator } from 'lucide-react';
 import useBudgetData from '@/hooks/useBudgetData';
 
 const BudgetView = ({ selectedDate }: { selectedDate: Date }) => {
-  const { budgets, isLoading } = useBudgetData(selectedDate);
+  const { totalBudget, categoryBudgets, isLoading } =
+    useBudgetData(selectedDate);
 
   if (isLoading)
     return (
@@ -12,9 +16,40 @@ const BudgetView = ({ selectedDate }: { selectedDate: Date }) => {
       </div>
     );
 
+  if (!totalBudget) {
+    return (
+      <AnalysisEmpty
+        title="이번 달 예산을 설정해 주세요"
+        description="지출을 관리하기 위해 먼저 한 달 총 예산을 정해볼까요?"
+        icon={Calculator}
+      >
+        <Button
+          variant="secondary"
+          className="bg-primary/5 hover:bg-primary/10 mt-2 cursor-pointer"
+        >
+          예산 설정하기
+        </Button>
+      </AnalysisEmpty>
+    );
+  }
+
   return (
     <div className="space-y-4">
-      <AnalysisSection title={'총 예산'}>총 예산</AnalysisSection>
+      <AnalysisSection title={'총 예산'}>
+        <div className="flex flex-col md:flex-row md:justify-center">
+          <div className="p-4">
+            <p className="text-muted-foreground text-base">이번 달 총 예산</p>
+            <p className="text-2xl font-bold">
+              {totalBudget.amount.toLocaleString()}원
+            </p>
+          </div>
+          <div className="p-4">
+            <p className="text-muted-foreground text-base">남은 예산</p>
+            <p className="text-2xl font-bold">????원</p>
+          </div>
+        </div>
+      </AnalysisSection>
+
       <AnalysisSection title={'카테고리별 예산'}>카테고리</AnalysisSection>
     </div>
   );
