9da1230857bec642edaa50b4b8497ad37afd4e78
윤진 <151013932+maruru301@users.noreply.github.com>
2026-01-04 15:11:42 +0900

    refactor: AnalysisEmpty 컴포넌트 재사용성 개선 #46
    - 재사용할 수 있도록 children 및 icon props 추가

--- fe/src/components/analysis/common/AnalysisEmpty.tsx
+++ fe/src/components/analysis/common/AnalysisEmpty.tsx
@@ -1,17 +1,24 @@
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import { ReceiptText } from 'lucide-react';
+import { LucideIcon, ReceiptText } from 'lucide-react';
+
+import { ReactNode } from 'react';
 
 type AnalysisEmptyProps = {
   title: string;
   description?: string;
+  icon?: LucideIcon;
+  children?: ReactNode;
 };
 
-const AnalysisEmpty = ({ title, description }: AnalysisEmptyProps) => {
+const AnalysisEmpty = ({
+  title,
+  description,
+  icon: Icon = ReceiptText,
+  children,
+}: AnalysisEmptyProps) => {
   return (
     <div className="flex flex-col items-center justify-center gap-4 py-12">
       <div className="bg-primary/5 rounded-full p-4">
-        <ReceiptText className="text-muted-foreground/80 h-8 w-8" />
+        <Icon className="text-muted-foreground/80 h-8 w-8" />
       </div>
 
       <div className="text-muted-foreground space-y-1 text-center">
@@ -19,13 +26,7 @@ const AnalysisEmpty = ({ title, description }: AnalysisEmptyProps) => {
         {description && <p className="text-sm">{description}</p>}
       </div>
 
-      <Button
-        variant="secondary"
-        className="bg-primary/5 hover:bg-primary/10 mt-2"
-        asChild
-      >
-        <Link href={'/'}>기록하러 가기</Link>
-      </Button>
+      {children && <div className="mt-2">{children}</div>}
     </div>
   );
 };
