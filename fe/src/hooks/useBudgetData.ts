f7582a752cd067051ca368bf2905d50b2af4a30f
윤진 <151013932+maruru301@users.noreply.github.com>
2026-01-04 15:16:26 +0900

    refactor: useBudgetData 훅에서 총 예산과 카테고리별 예산 분리 #46
    - category_id 존재 여부에 따라 totalBudget과 categoryBudgets로 분류

--- fe/src/hooks/useBudgetData.ts
+++ fe/src/hooks/useBudgetData.ts
@@ -5,7 +5,8 @@ import { fetchBudgets } from '@/services/analysis/budgetService';
 import { toast } from 'sonner';
 
 const useBudgetData = (selectedDate: Date) => {
-  const [budgets, setBudgets] = useState<Budget[]>([]);
+  const [totalBudget, setTotalBudget] = useState<Budget | null>(null);
+  const [categoryBudgets, setCategoryBudgets] = useState<Budget[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -15,10 +16,18 @@ const useBudgetData = (selectedDate: Date) => {
 
         const data = await fetchBudgets(selectedDate);
 
+        if (data) {
+          // category_id가 null이면 총 예산
+          const total = data.find((item) => item.category_id === null);
+          // category_id가 null이 아니면 카테고리별 예산
+          const categories = data.filter((item) => item.category_id !== null);
+
+          setTotalBudget(total || null);
+          setCategoryBudgets(categories);
+        }
         console.log(data); // 테스트용
-        setBudgets(data || []);
       } catch (err) {
-        console.error('[useBudget] 로딩 실패:', err);
+        console.error('예산 불러오기 실패:', err);
         const message =
           err instanceof Error ? err.message : '예산을 불러오지 못했습니다.';
         toast.warning(message);
@@ -30,7 +39,7 @@ const useBudgetData = (selectedDate: Date) => {
     fetchData();
   }, [selectedDate]);
 
-  return { budgets, isLoading };
+  return { totalBudget, categoryBudgets, isLoading };
 };
 
 export default useBudgetData;
