2a6f321412710db12560ac946c0df2d3c7d0abe2
HyeongjinKim98 <201721157@sangmyung.kr>
2026-01-06 04:07:04 +0900

    feat : calendar skeleton UI 적용

--- fe/src/app/(app)/page.tsx
+++ fe/src/app/(app)/page.tsx
@@ -6,6 +6,7 @@ import { formatMonth } from '@/utils/date';
 import { getMonthRange } from '@/utils/date';
 import { CalendarProvider } from '@/context/CalendarContext';
 import { TransactionProvider } from './history/TransactionContext';
+import { CalendarSkeleton } from '@/components/calendar/CalendarSkeleton';
 interface PageProps {
   searchParams: Promise<{
     month?: string;
@@ -29,7 +30,7 @@ export default async function Home({ searchParams }: PageProps) {
     <div className="flex w-full max-w-6xl">
       <TransactionProvider>
         <CalendarProvider>
-          <Suspense fallback={<div>skeleton</div>}>
+          <Suspense fallback={<CalendarSkeleton />}>
             <DataCalendar
               filters={filters}
               currentMonth={currentMonth}
