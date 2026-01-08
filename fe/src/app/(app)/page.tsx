37e543eccd323ad0c19d239b42de36a6fe8b17ec
HyeongjinKim98 <201721157@sangmyung.kr>
2026-01-08 17:24:16 +0900

    refactor : test ocr 제거 #58

--- fe/src/app/(app)/page.tsx
+++ fe/src/app/(app)/page.tsx
@@ -7,7 +7,6 @@ import { getMonthRange } from '@/utils/date';
 import { CalendarProvider } from '@/context/CalendarContext';
 import { TransactionProvider } from './history/TransactionContext';
 import { CalendarSkeleton } from '@/components/calendar/CalendarSkeleton';
-import TestOCR from '@/components/transaction/OCR';
 interface PageProps {
   searchParams: Promise<{
     month?: string;
@@ -40,7 +39,6 @@ export default async function Home({ searchParams }: PageProps) {
           </Suspense>
         </CalendarProvider>
       </TransactionProvider>
-      <TestOCR />
     </div>
   );
 }
