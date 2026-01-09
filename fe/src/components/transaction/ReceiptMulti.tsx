6e094ed9e5b549f6e1710293a5df8846ee898fb2
HyeongjinKim98 <201721157@sangmyung.kr>
2026-01-09 15:21:50 +0900

    feat : 영수증 이미지 상세보기 #58

--- fe/src/components/transaction/ReceiptMulti.tsx
+++ fe/src/components/transaction/ReceiptMulti.tsx
@@ -25,6 +25,8 @@ export default function ReceiptMulti({ onResult }: OCRProps) {
   const [files, setFiles] = useState<File[]>([]);
   const inputRef = useRef<HTMLInputElement>(null);
   const [previews, setPreviews] = useState<string[]>([]);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
   // 이미지를 base64 문자열로 변환
   const fileToBase64 = (file: File): Promise<string> => {
     return new Promise((resolve, reject) => {
@@ -63,66 +65,89 @@ export default function ReceiptMulti({ onResult }: OCRProps) {
     }
   };
   return (
-    <Dialog
-      open={open}
-      onOpenChange={(isOpen) => {
-        setOpen(isOpen);
-      }}
-    >
-      <DialogTrigger asChild>
-        <Button
-          className="h-16 w-16 cursor-pointer rounded-full hover:bg-gray-800"
-          asChild
-        >
-          {loading ? <Spinner /> : <Images size={12} />}
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="max-w-lg">
-        <DialogHeader>
-          <DialogTitle>영수증 업로드</DialogTitle>
-        </DialogHeader>
-        <Button onClick={() => inputRef.current?.click()}>
-          내 PC/갤러리에서 찾기
-        </Button>
-        <div className="cursor-pointer rounded-lg border-2 border-dashed p-16 text-center">
-          <Upload className="text-muted-foreground mx-auto mb-2" />
-          <p className="text-muted-foreground">
-            업로드 할 이미지를 드래그해주세요{' '}
-          </p>
-          <input
-            onChange={handleFiles}
-            ref={inputRef}
-            type="file"
-            accept="image/*"
-            multiple
-            hidden
-          />
-        </div>
-        {/* 미리보기 영역 */}
-        {previews.length > 0 && (
-          <>
+    <>
+      <Dialog
+        open={open}
+        onOpenChange={(isOpen) => {
+          setOpen(isOpen);
+        }}
+      >
+        <DialogTrigger asChild>
+          <Button
+            className="h-16 w-16 cursor-pointer rounded-full hover:bg-gray-800"
+            asChild
+          >
+            {loading ? <Spinner /> : <Images size={12} />}
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="max-w-lg overflow-hidden border-none">
+          <DialogHeader>
+            <DialogTitle>영수증 업로드</DialogTitle>
+          </DialogHeader>
+          <Button onClick={() => inputRef.current?.click()}>
+            내 PC/갤러리에서 찾기
+          </Button>
+          <div className="cursor-pointer rounded-lg border-2 border-dashed p-16 text-center">
+            <Upload className="text-muted-foreground mx-auto mb-2" />
             <p className="text-muted-foreground">
-              {previews.length}개의 이미지
+              업로드 할 이미지를 드래그해주세요{' '}
             </p>
-            <div className="grid grid-cols-4 gap-6 overflow-y-scroll">
-              {previews.map((src, index) => (
-                <div key={index} className="group relative">
-                  <img
-                    src={src}
-                    className="h-30 w-full rounded object-cover hover:opacity-60"
-                  />
-                  <button
-                    onClick={() => removeFile(index)}
-                    className="bg-destructive absolute top-1 right-1 cursor-pointer rounded-full p-1 text-white opacity-0 group-hover:opacity-60 hover:opacity-95"
-                  >
-                    <X size={16} />
-                  </button>
-                </div>
-              ))}
+            <input
+              onChange={handleFiles}
+              ref={inputRef}
+              type="file"
+              accept="image/*"
+              multiple
+              hidden
+            />
+          </div>
+          {/* 미리보기 영역 */}
+          {previews.length > 0 && (
+            <>
+              <p className="text-muted-foreground">
+                {previews.length}개의 이미지
+              </p>
+              <div className="grid grid-cols-4 gap-6 overflow-y-scroll">
+                {previews.map((src, index) => (
+                  <div key={index} className="group relative">
+                    <img
+                      src={src}
+                      onClick={() => setSelectedImage(src)}
+                      className="h-30 w-full rounded object-cover hover:opacity-60"
+                    />
+                    <button
+                      onClick={() => removeFile(index)}
+                      className="bg-destructive absolute top-1 right-1 cursor-pointer rounded-full p-1 text-white opacity-0 group-hover:opacity-60 hover:opacity-95"
+                    >
+                      <X size={16} />
+                    </button>
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
+          {/* 이미지 상세보기 overlay */}
+          {selectedImage && (
+            <div
+              className="round-lg absolute inset-0 flex items-center justify-center bg-black/70"
+              onClick={() => setSelectedImage(null)}
+            >
+              <Button
+                className="absolute bottom-4 h-16 w-16 cursor-pointer rounded-full text-white"
+                onClick={() => setSelectedImage(null)}
+                asChild
+              >
+                <X size={12} />
+              </Button>
+              <img
+                src={selectedImage}
+                className="max-h-96 max-w-96 object-contain"
+                onClick={(e) => e.stopPropagation()}
+              />
             </div>
-          </>
-        )}
-      </DialogContent>
-    </Dialog>
+          )}
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
