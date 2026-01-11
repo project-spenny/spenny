4f269da993ff4a0d5f8c033e8ef2be942cbabf3a
윤진 <151013932+maruru301@users.noreply.github.com>
2026-01-11 21:32:14 +0900

    feat: 헤더 로고에 홈 이동 링크 적용 #74

--- fe/src/components/layout/Header.tsx
+++ fe/src/components/layout/Header.tsx
@@ -2,6 +2,7 @@
 
 import GuestModeNotice from '../common/GuestModeNotice';
 import Image from 'next/image';
+import Link from 'next/link';
 import ModeToggle from '../common/ModeToggle';
 import { Profile } from '@/schemas/profile';
 import ProfilePanelTrigger from '../common/ProfilePanelTrigger';
@@ -25,12 +26,14 @@ const Header = () => {
   return (
     <header className="bg-secondary fixed top-0 z-10 flex h-16 w-full items-center justify-between border-b p-2">
       <h1 className="px-2">
-        <Image
-          src="/logo_horizontal.svg"
-          alt="Spenny"
-          width={200}
-          height={100}
-        />
+        <Link href="/" className="block">
+          <Image
+            src="/logo_horizontal.svg"
+            alt="Spenny"
+            width={200}
+            height={100}
+          />
+        </Link>
       </h1>
       <ModeToggle />
       {isGuest ? <GuestModeNotice /> : <ProfilePanelTrigger />}
