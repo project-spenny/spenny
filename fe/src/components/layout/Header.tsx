666969bcd7144a5387bd00d99e78e2559f138d54
윤진 <151013932+maruru301@users.noreply.github.com>
2026-01-11 22:25:21 +0900

    design: 네비게이션 브랜드 컬러 적용 및 헤더/푸터 배경색 변경 #74

--- fe/src/components/layout/Header.tsx
+++ fe/src/components/layout/Header.tsx
@@ -24,7 +24,7 @@ const Header = () => {
   const isGuest = !!profile?.is_guest;
 
   return (
-    <header className="bg-secondary fixed top-0 z-10 flex h-16 w-full items-center justify-between border-b p-2">
+    <header className="bg-background fixed top-0 z-10 flex h-16 w-full items-center justify-between border-b p-2">
       <h1 className="shrink-0 px-2 pt-1">
         <Link href="/">
           <Image
