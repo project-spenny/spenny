5e46deca164607f89124379ca305571c5b3d440d
HyeongjinKim98 <201721157@sangmyung.kr>
2026-01-12 11:45:07 +0900

    fix : input focus 디자인 수정 #71

--- fe/src/components/transaction/common/AmountInput.tsx
+++ fe/src/components/transaction/common/AmountInput.tsx
@@ -12,7 +12,7 @@ export const AmountInput = ({
     <div className="flex items-start gap-2">
       <Label className="w-26 pr-2">금액</Label>
       <div className="flex w-full flex-col gap-2">
-        <div className="flex items-center gap-2">
+        <div className="bg-background flex items-center gap-2">
           <Input
             id="amount"
             type="number"
@@ -20,7 +20,7 @@ export const AmountInput = ({
             value={value}
             onChange={(e) => onChange(e.target.value)}
             min="0"
-            className="h-12 flex-1 [appearance:textfield] rounded-none border-0 border-b-1 text-right !text-2xl [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+            className="focus-visible:border-brand h-12 flex-1 [appearance:textfield] rounded-none border-0 border-b-1 text-right !text-2xl focus-visible:ring-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
           />
           <span className="text-lg font-medium">원</span>
         </div>
