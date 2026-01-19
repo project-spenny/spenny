a7e85ee23efeee8ff345031eb404b1ec404cfdb7
aroin <bboroin917@gmail.com>
2026-01-19 12:42:42 +0900

    refactor: 로그인/로그아웃 시 profile 쿼리 캐시 제거 로직 정리#98

--- fe/src/components/my-info/LogoutButton.tsx
+++ fe/src/components/my-info/LogoutButton.tsx
@@ -3,16 +3,13 @@
 import { supabase } from '@/utils/supabase/client';
 import { useRouter } from 'next/navigation';
 import { Button } from '../ui/button';
-import { useQueryClient } from '@tanstack/react-query';
 
 export default function LogoutButton() {
   const router = useRouter();
-  const queryClient = useQueryClient();
 
   const handleLogout = async () => {
     await supabase.auth.signOut();
 
-    queryClient.removeQueries({ queryKey: ['profile'] });
     router.replace('/login'); // 로그아웃 후 로그인 페이지로 리다이렉트
     router.refresh();
   };
